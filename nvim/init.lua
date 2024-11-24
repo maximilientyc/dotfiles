83081879b0371035bbe77101ac10efb4b000425d
Maximilien Tyc <maximilien.tyc@gmail.com>
2024-11-24 12:08:00 +0100

    add function to run test

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+local run_test = function()
+  local current_file = vim.fn.expand('%:p')
+  return ":!zellij run -d Down -n 'testing' -- bin/rails test " .. current_file .. " <CR>"
+end
+
+vim.keymap.set("n", "<leader>n", run_test(), { desc = 'test current file' })
